cf93113191d39e247ce9c1514c6b5eb09b641510
SolaHasGit <solayun2@gmail.com>
2025-04-12 20:32:23 -0400

    fixed-config folder

--- services/supabaseService.js
+++ services/supabaseService.js
@@ -1,5 +1,5 @@
 const { createClient } = require('@supabase/supabase-js');
-const { SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY } = require('../config/env');
+const { SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY } = require('../config/dbConfig');
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
 
