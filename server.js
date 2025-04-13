0bbfc7f48a88879de6c5b39952a3d502f7b4400e
SolaHasGit <solayun2@gmail.com>
2025-04-12 21:33:52 -0400

    cleaning up directory

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const { getAuthToken } = require('./services/ebayAuth');  
 const path = require('path');
 
-const googleSheetsRouter = require('./routes/googleSheetsRoute');
 const ebayImageSearchRoutes = require('./routes/ebaySearchByImage');
 const ebaySearchRouter = require('./routes/ebaySearch');  
 const authRoutes = require('./routes/supabaseRoute');
@@ -14,8 +13,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;  
 
 // Middleware
-app.use(cors());  // Enable Cross-Origin Resource Sharing (CORS) if needed
-app.use(express.json());  // Parse incoming JSON requests
+app.use(cors());  
+app.use(express.json());  
 
 
 // Test route for eBay Auth token
@@ -30,9 +29,6 @@ app.get('/test-ebay-auth', async (req, res) => {
 
 // Searching for listings by keyword
 app.use('/api/search', ebaySearchRouter);  
-
-// Google Sheets Routes
-app.use('/api/sheets', googleSheetsRouter);
 
 // Searching for listings by image
 app.use('/api', ebayImageSearchRoutes);
